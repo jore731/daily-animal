c3b957b08437d331f7201ae01f876e4ed3ccdd20
Jorge Pulido <jorge.pulido@hotmail.com>
2025-02-11 18:52:54 +0000

    Aplicacion lista

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 import info
 
-st.title("Bienvenido a tu nuevas app!")
-st.write("Este es el punto de partida de tu nueva aplicación de Streamlit. A partir de aquí, puedes modificar este código para añadir nuevas funcionalidades y personalizar la aplicación a tu gusto.")
+st.title("Animales")
+st.header("elefante")
+
+columna_izquierda, columna_derecha = st.columns(2)
+
+with columna_izquierda:
+  st.image(info.foto_elefante)
+with columna_derecha:
+  pais1, pais2, pais3, pais4 = st.columns(4, vertical_alignment="center")
+  
+  with pais1:
+      st.image(info.bandera_kenya, caption="Kenya")
+  with pais2:
+      st.image(info.bandera_tanzania, caption="Tanzania")
+  with pais3:
+      st.image(info.bandera_zimbabwe, caption="Zimbabwe")
+  with pais4:
+      st.image(info.bandera_bostwana, caption="Botswana")
+
+  st.write(f"**Descripción** {info.descripcion_elefante}")
+  st.map(info.coordenadas_elefante,height=300,zoom=1,size=1000000)
